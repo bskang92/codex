a406ad804c6b5d26bdfe5621aa2016a4e7134cdc
홍길동 <your@email.com>
2025-06-02 15:15:34 +0900

    불필요한 파일 삭제

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.feature-card');
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('active');
-        });
-    });
-});
